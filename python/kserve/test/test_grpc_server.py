8202f172c38c675b62432ecf6d686fd9bfee639d
Sivanantham <90966311+sivanantha321@users.noreply.github.com>
2024-08-26 11:09:16 +0530

    Update python/kserve/test/test_grpc_server.py
    Signed-off-by: Sivanantham <90966311+sivanantha321@users.noreply.github.com>

--- python/kserve/test/test_grpc_server.py
+++ python/kserve/test/test_grpc_server.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import grpc
-import grpc_testinga  # noqa F821
+import grpc_testingsa, grpc_tesing  # noqa F821
 import numpy as np
 import pandas as pd
 import pytest
